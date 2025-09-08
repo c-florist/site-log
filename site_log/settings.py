09e492486fba5acb13be465b6c7935980fc13cdb
Connor Flowers <cjflowers@pm.me>
2025-09-08 16:35:55 +1000

    add basic rest framework permissions

--- site_log/settings.py
+++ site_log/settings.py
@@ -68,6 +68,14 @@ LOGGING = {
     },
 }
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
